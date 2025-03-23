ae053ed3c120fae79e74bfd527541359667dba08
in0ho1no <yin0ho1no@gmail.com>
2025-03-23 13:55:59 +0000

    ビルド手順の追記

---
+++ work_dir/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd ../hcpworks
+
+# CHANGELOG の更新確認
+echo "Did you update the CHANGELOG? (y/n)"
+read answer
+
+# ユーザーの入力に応じて処理を続行または中断
+if [ "$answer" == "n" ]; then
+    echo "Process aborted."
+    exit 1
+fi
+
+# パッケージングする
+npx vsce package
