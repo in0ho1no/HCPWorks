e991f6790562d23e7da6ad1d24813f625cfcb236
in0ho1no <yin0ho1no@gmail.com>
2025-05-05 06:03:34 +0000

    色指定が分かりやすいように変更 ・文字列からRRGGBB形式の指定へ

--- hcpworks/src/render/render_define.ts
+++ hcpworks/src/render/render_define.ts
@@ -1,5 +1,5 @@
 export class DiagramDefine {
-  static readonly DEFAULT_BG_COLOR = "ffffff"; // デフォルトの背景色
+  static readonly DEFAULT_BG_COLOR = "FFFFFF"; // デフォルトの背景色
 
   static readonly LEVEL_SHIFT = 30;   // レベルの差を表す水平方向のシフト
 
@@ -7,13 +7,13 @@ export class DiagramDefine {
 
   static readonly LINE_OFFSET = 10;
   static readonly COLOR_TABLE = [
-    "black",
-    "red",
-    "green",
-    "blue",
-    "yellow",
-    "purple",
-    "orange",
-    "turquoise",
+    "000000", // black
+    "FF0000", // red
+    "00FF00", // green
+    "0000FF", // blue
+    "FFFF00", // yellow
+    "800080", // purple
+    "FFA500", // orange
+    "40E0D0", // turquoise
   ];
 }
